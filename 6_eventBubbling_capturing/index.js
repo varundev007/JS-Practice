47d423f1c5961cef0c5434eb4368e8e98b7f9a4b
varun <varundwivedi6@gmail.com>
2025-03-27 21:27:45 +0530

    Updated

---
+++ 6_eventBubbling_capturing/index.js
@@ -0,0 +1,47 @@
+// document.querySelector("#grandparent")
+//  .addEventListener('click', () => {
+//     console.log("Grandparent clicked")
+//  })
+
+//  document.querySelector("#parent")
+//  .addEventListener('click', () => {
+//     console.log("parent clicked")
+//  })
+
+//  document.querySelector("#child")
+//  .addEventListener('click', () => {
+//     console.log("child clicked")
+//  })
+
+//same as above
+
+// document.querySelector("#grandparent")
+//  .addEventListener('click', () => {
+//     console.log("Grandparent clicked")
+//  }, false)
+
+//  document.querySelector("#parent")
+//  .addEventListener('click', () => {
+//     console.log("parent clicked")
+//  }, false)
+
+//  document.querySelector("#child")
+//  .addEventListener('click', () => {
+//     console.log("child clicked")
+//  }, false)
+
+document.querySelector("#grandparent")
+ .addEventListener('click', () => {
+    console.log("Grandparent clicked")
+ }, true)
+
+ document.querySelector("#parent")
+ .addEventListener('click', () => {
+    console.log("parent clicked")
+ }, true)
+
+ document.querySelector("#child")
+ .addEventListener('click', () => {
+    console.log("child clicked")
+ }, true)
+
